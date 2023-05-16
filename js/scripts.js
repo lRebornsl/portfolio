9becdc111c30fb8200735694963cff14b96bec54
lRebornsl <anthony_vasquez98@hotmail.com>
2023-05-15 19:37:19 -0500

    Added new linters and modified JS file.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ const mobileNav = document.getElementById('nav');
 const logo = document.getElementById('logo');
 const barImg = document.getElementById('bar-img');
 
-btnNav.addEventListener('click', function(e){
-    if(mobileNav.classList.contains('hidden')) {
-        barImg.classList.add('bar-img');
-        mobileNav.classList.remove('hidden');
-        logo.classList.add('hidden');
-    } else {
-        barImg.classList.remove('bar-img');
-        mobileNav.classList.add('hidden');
-        logo.classList.remove('hidden');
-    }
-})
+btnNav.addEventListener('click', () => {
+  if (mobileNav.classList.contains('hidden')) {
+    barImg.classList.add('bar-img');
+    mobileNav.classList.remove('hidden');
+    logo.classList.add('hidden');
+  } else {
+    barImg.classList.remove('bar-img');
+    mobileNav.classList.add('hidden');
+    logo.classList.remove('hidden');
+  }
+});
