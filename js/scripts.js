e89f89f65c309307b172932a41413cf6ec8d84b6
lRebornsl <anthony_vasquez98@hotmail.com>
2023-05-15 19:20:03 -0500

    Modified nav bar and added new icon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-const btnNav = document.querySelector('.bar-button');
-const a = document.getElementById('nav');
+const btnNav = document.querySelector('.mobile-bar');
+const mobileNav = document.getElementById('nav');
+const logo = document.getElementById('logo');
+const barImg = document.getElementById('bar-img');
 
 btnNav.addEventListener('click', function(e){
-    a.classList.remove("hidden");
+    if(mobileNav.classList.contains('hidden')) {
+        barImg.classList.add('bar-img');
+        mobileNav.classList.remove('hidden');
+        logo.classList.add('hidden');
+    } else {
+        barImg.classList.remove('bar-img');
+        mobileNav.classList.add('hidden');
+        logo.classList.remove('hidden');
+    }
 })
